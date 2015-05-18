237f1e4f054d72b63622b9d809ede1a5424a4fb7
dlegland <david.legland@grignon.inra.fr>
2015-05-18 18:52:56 +0200

    add functions oneBlock.m and scalarBlock.m

---
+++ multiBlock/scalarBlock.m
@@ -0,0 +1,25 @@
+function BM = scalarBlock(mat)
+%SCALARBLOCK  Converts a matrix to a BlockMatrix with only scalar blocks
+%
+%   BM = scalarBlock(MAT)
+%
+%   Example
+%   BM = oneBlock(magic(3));
+%   reveal(BM)
+%          1  1  1
+%       1  +  +  +
+%       1  +  +  +
+%       1  +  +  +
+%
+%   See also
+%   BlockMatrix, oneBlock
+ 
+% ------
+% Author: David Legland
+% e-mail: david.legland@nantes.inra.fr
+% Created: 2015-05-18,    using Matlab 8.4.0.150421 (R2014b)
+% Copyright 2015 INRA - Cepia Software Platform.
+
+n = size(mat, 1);
+p = size(mat, 2);
+BM = BlockMatrix(mat, ones(1, n), ones(1, p));
