b20559056eee2a6510c0a73b816ecaf62b36b107
dlegland <david.legland@nantes.inra.fr>
2015-07-13 18:00:55 +0200

    add maxbet procedure demo

---
+++ multiBlock/maxbet_procedure2.m
@@ -0,0 +1,63 @@
+function [q, iter, resid] = maxbet_procedure2(data, tt, tol)
+%MAXBET_PROCEDURE2  MAXBET procedure for multi-block matrices.
+%
+%   Q = maxbet_procedure1(DATA, TT, TOL)
+%   Computes the solution of the original maxbet problem, that consists in
+%   maximizing: (Q' * XX' * XX * Q), under block-normalized vector 
+%   (norm(qk) = 1).
+%
+%   [Q, ITER] = maxbet_procedure1(DATA, TT, TOL)
+%   Also returns the number of iterations.
+%
+%   Inputs:
+%   DATA:   multiblocks input matrix as horizontal list of blocks
+%   TT:     block vector as horizontal list of block vectors (not
+%           necessarily normalized by blocks)
+%   TOL:    convergence tolerance
+%
+%   Outputs:
+%   Q:      solution, as a list of horizontal block vectors
+%   ITER:   the number of iterations needed to reach convergence
+%
+%
+%   Example
+%     % Create Block-matrix for the data
+%     data = BlockMatrix(rand(4, 7), 4, [2 3 2]);
+%     % Create Block-matrix for initialisation vector
+%     tt = BlockMatrix(rand(7, 1), [2 3 2], 1);
+%     % call the MAXBET procedure
+%     [q, iter] = maxbet_procedure1(data, tt, 1e-3);
+%     disp(sprintf('converged afeter %d iterations', iter));
+%
+%
+%   See also
+%     BlockMatrix
+%
+%   References
+%   Van De Geer (1984), Ten Berge (1988), Hanafi and Kiers (2006)
+%
+
+% create new BlockMatrix representing the normalized input vectors
+qq = blockProduct_hs(1./norm(tt), tt);
+
+AA = blockProduct_uu(data',data);
+
+resid = 1;
+
+iter = 0;
+
+while resid > tol
+    
+    iter = iter + 1;
+    
+    q = blockProduct_uu(AA,qq);
+    
+    q = blockProduct_hs(1./norm(q), q); % block normalization
+    
+    resid = norm(norm(q)-norm(qq));% residual
+    
+    qq = q;
+end
+
+
+ 
---
+++ multiBlock/test_maxbet2.m
@@ -0,0 +1,53 @@
+%TEST_MAXBET2 Programme de test pour la fonction 'maxbet_procedure2'
+%
+%   Usage: test_maxbet2
+%
+%   Example
+%   test_maxbet2
+%
+%   See also
+%   test_maxbet1
+ 
+%% Cree une bloc-matrice pour les donnees
+% On part sur une bloc-matrice de 3 blocs concatenes en ligne, 
+% avec des tailles de bloc de 2, 3 et 2 colonnes, 
+% et 4 lignes pour chaque bloc.
+% La matrice equivalente a une taille de 4-par-7.
+
+% create block dimensions
+mdims = BlockDimensions({4, [2 3 2]});
+
+% create block matrix instance
+data = BlockMatrix(rand(4, 7), mdims);
+
+% display the BlockMatrix
+disp('Block matrix:');
+disp(data);
+% displayData(data);
+
+
+%% Cree une bloc-matrice pour les vecteurs
+% On utilise une bloc-matrice de 3 blocs, chaque bloc contenant un vecteur.
+% La longueur des vecteurs est de (2,3,2).
+
+% create block dimensions
+vdims = BlockDimensions({[2 3 2], 1});
+
+% create block matrix instance
+tt = BlockMatrix(rand(7, 1), vdims);
+
+% display the block-vector (transposed)
+disp('Transpose of input vector t:');
+disp(tt');
+% displayData(tt');
+
+
+%% Run the maxbet procedure
+
+% sample line to run the procedure
+q = maxbet_procedure2(data, tt, .01);
+
+% display the result (transposed)
+disp('Transpose of result q:');
+disp(q');
+% displayData(q')
